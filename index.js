10edbf6111a82346caab6977cf115a9a6cbc2aa5
Calvin Metcalf <cmetcalf@appgeo.com>
2013-12-03 08:21:51 -0500

    update code based on geopouch

--- index.js
+++ index.js
@@ -1,58 +1,34 @@
-function geometryVolume(geometry){
-  var mins = [];
-  var maxes = [];
-  function test(coordinate){
-    coordinate.forEach(function(v, i){
-      if(i>mins.length){
-        mins.push(v);
-      }else if(mins[i]>v){
-          mins[i] = v;
-      }
-      if(i>maxes.length){
-        maxes.push(v);
-      }else if(maxes[i]<v){
-        maxes[i] = v;
-      }
+function caclulateVolume(geom) {
+  var coords = geom.coordinates;
+  if (geom.type === 'Point') {
+    return [coords, coords];
+  }
+  if (geom.type === 'GeometryCollection') {
+    coords = geom.geometries.map(function(g) {
+      return caclulateVolume(g);
     });
-  }
-  function innerTest(coords){
-    coords.forEach(test);
-  }
-  switch(geometry.type){
-    case 'Point':
-      test(geometry.coordinates);
-      break;
-    case 'MultiPoint':
-    case 'LineString':
-      geometry.coordinates.forEach(test);
-      break;
-    case 'MultiLineString':
-    case 'Polygon':
-      geometry.coordinates.forEach(innerTest);
-      break;
-    case 'MultiPolygon':
-      geometry.coordinates.forEach(function(coords){
-        coords.forEach(innerTest);
-      });
-      break;
-    case 'GeometryCollection':
-      geometries.forEach(function(geome){
-        geometryVolume(geom).forEach(test);
-      });
-      break;
-  }
-  return [mins,maxes];
-}
 
-module.exports = function(feature){
-  if(feature.type === 'Feature'){
-    return geometryVolume(feature.geometry);
-  }else if(feature.type === 'FeatureCollection'){
-    return geometryVolume({
-      type:'GeometryCollection',
-      geometries:feature.features.map(function(v){
-        return v.geometry;
-      });
+  // Flatten coords as much as possible
+  while (Array.isArray(coords[0][0])) {
+    coords = coords.reduce(function(a, b) {
+      return a.concat(b);
+    });
+  };
+
+  // Calculate the enclosing bounding box of all coordinates
+  return coords.reduce(function (acc, coord) {
+    if (acc === null) {
+      return [coord, coord];
+    }
+    return [
+    acc[0].map(function(a,i){
+      return Math.min(a,coord[i]);
+    }),
+    acc[1].map(function(a,i){
+      return Math.max(a,coord[i]);
     })
-  }
-}
+    ];
+  }, null);
+};
+
+module.exports = caclulateVolume;
